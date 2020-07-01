787fc48f659c5885791444f06c771b10de553751
Murat MERCAN <mmercan@outlook.com>
2020-07-01 14:26:59 +1000

    docker files added

--- runner.ps1
+++ runner.ps1
@@ -6,7 +6,7 @@ $dir = Split-Path $scriptpath
 
 
 if ($param -eq "build") {
-    docker-compose.exe -f docker-compose.yml up --build --force-recreate cookbook-admin-ui cookbook-recipe-ui cookbook-comms-api cookbook-recipe-api cookbook-db-mongodb cookbook-redis cookbook-rabbitmq
+    docker-compose.exe -f docker-compose.yml up --build --force-recreate cookbook-recipe-api cookbook-comms-api cookbook-comms-handler cookbook-recipe-handler #cookbook-admin-ui cookbook-recipe-ui # cookbook-db-mongodb  cookbook-rabbitmq cookbook-redis 
 }
 else {
     docker-compose.exe -f docker-compose.yml up  cookbook-admin-ui cookbook-recipe-ui cookbook-comms-api cookbook-recipe-api cookbook-db-mongodb cookbook-redis cookbook-rabbitmq
