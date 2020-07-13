7ac452a1aaab0b240d771b740c3eb22c82dafed4
Murat MERCAN <mmercan@outlook.com>
2020-07-13 20:36:34 +1000

    swagger and kubeclient added

---
+++ CookBook.Comms.Api/Controllers/KubeClientTestController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using k8s;
+
+namespace CookBook.Comms.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KubeClientTestController : ControllerBase
+    {
+
+        ILogger<KubeClientTestController> _logger;
+
+        public KubeClientTestController(ILogger<KubeClientTestController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public object Get()
+        {
+
+           // var config = KubernetesClientConfiguration.BuildDefaultConfig();
+            var config = KubernetesClientConfiguration.InClusterConfig();
+            IKubernetes client = new Kubernetes(config);
+            Console.WriteLine("Starting Request!");
+
+            var list = client.ListNamespacedPod("cookbook-dev");
+            foreach (var item in list.Items)
+            {
+                Console.WriteLine(item.Metadata.Name);
+            }
+            if (list.Items.Count == 0)
+            {
+                Console.WriteLine("Empty!");
+            }
+            return list.Items;
+
+            //return View();
+        }
+    }
+}
--- CookBook.Comms.Api/Startup.cs
+++ CookBook.Comms.Api/Startup.cs
@@ -10,6 +10,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using CookBook.Common;
+using System.IO;
 
 namespace CookBook.Comms.Api
 {
@@ -26,17 +35,62 @@ namespace CookBook.Comms.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+             services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+            });
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen(options =>
+            {
+                options.OperationFilter<SwaggerDefaultValues>();
+                options.IncludeXmlComments(XmlCommentsFilePath);
+
+                options.AddSecurityDefinition("BearerAuth", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "Bearer "
+                });
+
+            });
+
+              services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration["RedisConnection"];
+                options.InstanceName = "ApiComms";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,  ILoggerFactory loggerFactory, IApiVersionDescriptionProvider provider, IHostApplicationLifetime lifetime, IDistributedCache cache)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
+
+            app.UseSwagger(e => { e.AddHealthCheckSwaggerOptions(); });
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
@@ -46,6 +100,17 @@ namespace CookBook.Comms.Api
             {
                 endpoints.MapControllers();
             });
+        }
+
+                static string XmlCommentsFilePath
+        {
+            get
+            {
+                //var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var basePath = AppContext.BaseDirectory;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine(basePath, fileName);
+            }
         }
     }
 }
